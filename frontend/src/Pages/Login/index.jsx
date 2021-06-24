3eca79880c00799ec7b3f9169dee4e1d3a2c2619
arthur teixeira <arthurcarvalho24@gmail.com>
2021-06-24 19:15:06 -0300

    - Added Login page

---
+++ frontend/src/Pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { LoginModal } from '../../Components'
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginModal/>      
+    </>
+  );
+}
+
+export default LoginPage;
