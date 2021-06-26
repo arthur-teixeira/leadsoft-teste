ec703375d59c82094afc2b039fe0b190cd947848
arthur teixeira <arthurcarvalho24@gmail.com>
2021-06-26 14:01:30 -0300

    Added array mapping

--- frontend/src/Components/PersonCardGrid/index.jsx
+++ frontend/src/Components/PersonCardGrid/index.jsx
@@ -5,20 +5,48 @@ import styles from './PersonCardGrid.module.css'
 import { PersonCard } from '../'
 
 const PersonCardGrid = () => {
+
+  const peopleData = [
+    {
+      name: "Arthur Teixeira",
+      age: 18,
+      weight: 50,
+      height: 1.8,
+      id: "ijahsdfkah2891734"
+    },
+    {
+      name: "Arthur Carvalho",
+      age: 18,
+      weight: 50,
+      height: 1.8,
+      id: "kajshfnkjszhg"
+    },
+    {
+      name: "Julia Kalil",
+      age: 17,
+      weight: 50,
+      height: 1.8,
+      id: "huçlghuklg"
+    },
+    {
+      name: "Angelita",
+      age: 50,
+      weight: 80,
+      height: 1.7,
+      id: "wa13125515"
+    },
+    {
+      name: "Arthur Teixeira",
+      age: 18,
+      weight: 50,
+      height: 1.8,
+      id: "asday34723"
+    },
+  ]
+
   return (
     <div className={styles.GridWrapper}>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
-      <PersonCard/>
+      {peopleData.map(personData => <PersonCard personData={personData} key={personData.id}/>)}
     </div>
   );
 }
